6ccd36937585e46845988ea483335fa36b7397f8
Swapney Raul <raulswapney97@gmail.com>
2025-10-14 18:05:26 +0530

    Implement calculation and display answer
    - Create function to get number pressed
    - Create function to get operator pressed
    - Add fucntion to do operations on selected nums and operators

--- script.js
+++ script.js
@@ -1,27 +1,70 @@
+// DOM variables
+const btnContainer = document.querySelector(".lower");
+const inputField = document.querySelector(".input-field");
+const result = document.querySelector(".result");
+
 // variables to store userinputs
 let num1;
 let num2;
 let operator;
 
+let numClicked;
+let operatorClicked;
+let isOperatorClicked;
+
 // obj to store operations
 const calcObj = {
-  '+': function (a, b) {
+  "+": function (a, b) {
     return a + b;
   },
-  '-': function (a, b) {
+  "-": function (a, b) {
     return a - b;
   },
-  '*': function (a, b) {
+  "x": function (a, b) {
     return a * b;
   },
-  '/': function (a, b) {
+  "÷": function (a, b) {
     return a / b;
   },
 };
 
 // takes in user input and does operations on it
-function operate(num1, num2, operator){
-    return calcObj[operator](num1, num2);
+function operate(num1, num2, operator) {
+  return calcObj[operator](num1, num2);
 }
 
-console.log(operate(5, 7, '+'));
+// get the number pressed, store and display
+function getNumClicked() {
+  btnContainer.addEventListener("click", (e) => {
+    if (e.target.matches(".numbers")) {
+      numClicked = e.target.textContent;
+      inputField.value += numClicked;
+      console.log(numClicked);
+    }
+  });
+}
+getNumClicked();
+
+// perform operations and get result
+function getOperationClicked() {
+  btnContainer.addEventListener("click", (e) => {
+    if (e.target.matches(".operator")) {
+      operatorClicked = e.target.textContent;
+      isOperatorClicked = true;
+      num1 = Number(inputField.value);
+      inputField.value = "";
+      console.log(num1);
+    }
+  });
+}
+getOperationClicked();
+
+function getResult() {
+  result.addEventListener("click", () => {
+    num2 = Number(inputField.value);
+    let answer = operate(num1, num2, operatorClicked);
+    num1 = answer;
+    return inputField.value = answer;
+  });
+}
+getResult();
